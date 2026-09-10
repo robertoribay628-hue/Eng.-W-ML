af3ccb19fe867729f94f8c91c3c6e5e84e822a47
robertoribay628-hue <robertoribay628@gmail.com>
2026-09-09 21:11:29 -0400

    Add files via upload
    Versions of questions 4-6 for HW 1 of MANE 4961 Engineering with Machine Learning that use pytorch instead of tensorflow. This code is not used in the final version of the homework problems as it was deemed better to use the in-class code with slight modifications. Though they are unused, they (should) be in a functional state and can be used in the future for classification or regression problems, hence why they are being uploaded.

---
+++ Question5.py
@@ -0,0 +1,102 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from torch.utils.data import TensorDataset, DataLoader
+import numpy as np
+import random
+
+# Setting up the neural network infrastructure
+device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+print(f"Using {device} device")
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(1, 256),
+            nn.ReLU(),
+            nn.Linear(256, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 1),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+model = NeuralNetwork()
+loss_fn = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, mae = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            mae += torch.abs(pred - y).sum().item()
+    test_loss /= num_batches
+    mae /= size
+    print(f"Test Error: \n Mean Absolute Error: {(mae):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+# Importing, loading, and splitting data and training sets
+num_nums = 100
+X = np.zeros([num_nums])
+y = np.zeros([num_nums])
+for i in range(num_nums):
+    X[i] = random.randint(1,100)
+    y[i] = X[i] ** (1 / 7)
+
+
+print(X,y)
+
+train_X, val_X, train_y, val_y = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=123)
+
+train_X_t = torch.tensor(train_X, dtype=torch.float32).reshape(-1, 1)
+train_y_t = torch.tensor(train_y, dtype=torch.float32).reshape(-1, 1)
+val_X_t = torch.tensor(val_X, dtype=torch.float32).reshape(-1, 1)
+val_y_t = torch.tensor(val_y, dtype=torch.float32).reshape(-1, 1)
+
+train_dataset = TensorDataset(train_X_t, train_y_t)
+test_dataset = TensorDataset(val_X_t, val_y_t)
+
+traindata = DataLoader(train_dataset, batch_size=16, shuffle=True)
+testdata = DataLoader(test_dataset, batch_size=16)
+
+# Training the neural network
+epochs = 50
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(traindata, model, loss_fn, optimizer)
+    test(testdata, model, loss_fn)
+print("Done!")
